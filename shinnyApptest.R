061b7d097f4810d6c5717c21565ce1568baf608b
ManickLeolin <manicleolin2@gmail.com>
2025-02-06 18:54:00 +0100

    Debut de création d'une application shiny

---
+++ shinnyApptest.R
@@ -0,0 +1,8 @@
+usethis::use_git_config(
+  scope = "user",
+  user.name = "ManickLeolin",
+  user.email = "manicleolin2@gmail.com",
+  init.defaultbranch = "main"
+)
+install.packages("shiny")
+library(shiny)
